7ed7bdfb740aad903830996f23318c49dd46beb8
Sergei Mikhailenko <sersus1991@gmail.com>
2022-12-09 21:19:14 +0200

    add zero components and box, thema

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { Box } from './Common/Box';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return (
+      <Box>
+        <Box></Box>
+        React homework template
+      </Box>
+    );
+  }
+}
